7732438256acc7f164c96600748771dc719c79f1
Ssewagudde David <dsewagude10@gmail.com>
2023-02-16 20:09:09 +0300

    repos

--- app.js
+++ app.js
@@ -1,36 +1,32 @@
-// Init github
-const github = new GitHub;
+// Init Github
+const github = new Github;
 
-// init UI
+// Init UI
 const ui = new UI;
-// Searchinput
+
+// Search input
 const searchUser = document.getElementById('searchUser');
 
 // Search input event listener
 searchUser.addEventListener('keyup', (e) => {
-    // Get input text
-    const userText = e.target.value;
+  // Get input text
+  const userText = e.target.value;
 
-    if(userText !== '') {
-        // Make http call
-        github.getUser(userText)
-            .then(data => {
-                if(data.profile.message === 'Not found') {
-                    // Show alert
-                    ui.showAlert('User not found', 'alert alert-danger')
-
-                } else {
-                    // Show profile
-                    ui.showProfile(data.profile);
-                }
-            })
-    } else {
-        // Clear profile
-        ui.clearProfile();
-    }
-})
-
-
-
-
-
+  if(userText !== ''){
+   // Make http call
+   github.getUser(userText)
+    .then(data => {
+      if(data.profile.message === 'Not Found') {
+        // Show alert
+        ui.showAlert('User not found', 'alert alert-danger');
+      } else {
+        // Show profile
+        ui.showProfile(data.profile);
+        ui.showRepos(data.repos);
+      }
+    })
+  } else {
+    // Clear profile
+    ui.clearProfile();
+  }
+}); 
--- github.js
+++ github.js
@@ -1,16 +1,22 @@
-class GitHub {
+class Github {
     constructor() {
         this.client_id = '3c021d352d37b31614b4';
         this.client_secret = '3ed9b29b0ecf509280eda0a8a1026a86c0d3b24e';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
 
     async getUser(user) {
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
         const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
 
         return {
-            profile
+            profile,
+            repos
         }
     }
 }
